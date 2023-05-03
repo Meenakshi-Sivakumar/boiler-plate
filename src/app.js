26b978c35ba7c0c9e957b0353b105a4a6c4b1136
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-03 10:26:07 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import '../node_modules/normalize.css'
+
+
+ReactDOM.render(<p>This is my boilerPlate</p>,document.getElementById('app')); 
